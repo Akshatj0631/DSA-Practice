a63b637813f6ffc697e150ded80313a6540e1b48
Akshatj0631 <akshatgautam107@gmail.com>
2026-09-25 16:26:03 +0530

    Add solution for Search in Rotated Sorted Array

---
+++ 0033-search-in-rotated-sorted-array/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+public int search(int[] nums, int target) {
+int n=nums.length;
+int left=0,right=n-1;
+while(left<=right)
+{
+int mid=(left+right)>>>1;
+if(nums[mid]==target) return mid;
+// left sort
+if(nums[mid]>=nums[left])
+{
+if(target<nums[mid] && target>=nums[left])right=mid-1;
+else left=mid+1;
+}
+//right sort
+else{
+//target bet mid and right go for right else left
+if(target>nums[mid] && target<=nums[right]) left=mid+1;
+else right=mid-1;
+}
+}
+return -1;
+}
+}
